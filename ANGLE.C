4783ab7fb96b61ece833a490f1cd00c09e2a98ea
vpatel1161 <vrushti866@gmail.com>
2024-07-18 19:37:25 +0530

    Scanf Changes

--- ANGLE.C
+++ ANGLE.C
@@ -1,8 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
 void main(){
-int a1=65,a2=45,a3;
+int a1,a2,a3;
 clrscr();
+printf("Angle of a1");
+scanf("%d",&a1);
+printf("Angle of a1:");
+scanf("%d",&a2);
 a3=180-(a1+a2);
 printf("angle of a3:%d",a3);
 getch();
